dae2e00e83bc7ca850f40013e5e80b564b9652ae
Erin Woodard <79154737+erinwoodard8@users.noreply.github.com>
2021-03-24 17:53:51 -0400

    Add files via upload

---
+++ Components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/footer.css'
+
+function Footer() {
+    return (
+        <footer className="footer">
+                <p>2021 © copyright</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ Components/header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Navbar, Nav} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
+import '../css/header.css'
+
+const header = () => {
+    return (
+        <Navbar className="navBar" expand="lg" >
+            <LinkContainer to="/">
+  <Navbar.Brand className="navBarHome">Pet World</Navbar.Brand>
+  </LinkContainer>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="navBarLinks">
+        <LinkContainer to="/products">
+      <Nav.Link className="productLink">Available Animals</Nav.Link>
+      </LinkContainer>
+      <LinkContainer to="/about">
+      <Nav.Link className="aboutLink">About Us</Nav.Link>
+      </LinkContainer>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+    )
+}
+
+export default header
